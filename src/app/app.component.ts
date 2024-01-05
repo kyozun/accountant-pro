027dd7d6e3e4950b249a99e1b538a0d5e49012ed
kyozun <daicacuong0311@gmail.com>
2024-01-05 09:54:15 +0700

    Add PrimeNg Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {ButtonModule} from "primeng/button";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, DropdownModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'accountant-pro-angular';
+
+  cities: City[] | undefined;
+
+  selectedCity: City | undefined;
+
+  ngOnInit() {
+    this.cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome', code: 'RM'},
+      {name: 'London', code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris', code: 'PRS'}
+    ];
+  }
 }
