65debc6d4d5520503dc03c14a770d85590dde905
kyozun <daicacuong0311@gmail.com>
2024-01-08 19:10:28 +0700

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,128 @@
-import {Component, OnInit} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
-import {ButtonModule} from "primeng/button";
-import {DropdownModule} from "primeng/dropdown";
-import {FormsModule} from "@angular/forms";
-
-interface City {
-  name: string;
-  code: string;
-}
+import { Component, Inject, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button'
+import { MAT_DIALOG_DATA, MatDialog, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog'
+import {MatCardModule} from "@angular/material/card";
+import {MatRippleModule} from "@angular/material/core";
+import {MatListModule} from "@angular/material/list";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule, DropdownModule, FormsModule],
+  imports: [CommonModule, RouterOutlet, MatIconModule, MatSidenavModule, MatCheckboxModule, FormsModule, MatButtonModule, MatInputModule, MatListModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-
-
 export class AppComponent implements OnInit {
-  title = 'accountant-pro-angular';
+  events: string[] = []
+  opened: boolean = true
 
-  cities: City[] | undefined;
+  title = 'accountant-pro-angular'
 
-  selectedCity: City | undefined;
+  ngOnInit() {}
 
-  ngOnInit() {
-    this.cities = [
-      {name: 'New York', code: 'NY'},
-      {name: 'Rome', code: 'RM'},
-      {name: 'London', code: 'LDN'},
-      {name: 'Istanbul', code: 'IST'},
-      {name: 'Paris', code: 'PRS'}
-    ];
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogDataExampleDialog, {
+      data: {
+        animal: 'panda',
+      },
+      width: '800px',
+    })
   }
+}
+
+@Component({
+  selector: 'dialog-data-example-dialog',
+  template: `
+    <div class="p-4 ">
+      <div class="text-xl font-bold">Trích xuất giấy tờ tùy thân</div>
+      <div class="flex flex-row space-x-1 my-2">
+        <div class="text-blue-600 font-medium">1. Tải lên ảnh</div>
+        <mat-icon fontIcon="chevron_right"></mat-icon>
+        <div>2. Kiểm tra thông tin</div>
+      </div>
+
+      <div class="my-2 flex flex-row space-x-4">
+        <button mat-raised-button mat-button color="primary">CMND/CCCD</button>
+        <button mat-raised-button mat-button color="basic">Hộ chiếu</button>
+      </div>
+
+      <hr class="my-4 h-px border-0 bg-gray-200 " />
+
+      <div class="flex flex-row items-center space-x-2">
+        <div class="inline-flex h-9 w-9 items-center justify-center rounded-full bg-blue-200 ">
+          <mat-icon aria-hidden="false" color="primary" aria-label="Example home icon" fontIcon="photo_camera"></mat-icon>
+        </div>
+        <div class="text-sm font-bold">Tải lên ảnh CMDND/CCCD</div>
+      </div>
+
+      <div class="my-4 grid grid-cols-2 gap-4">
+        <div class="col-span-1 ">
+          <mat-card>
+            <mat-card-header>
+              <mat-card-subtitle>
+                <div class="font-bold text-blue-600">Mặt trước</div>
+              </mat-card-subtitle>
+              <mat-card-title>Hoặc kéo ảnh mặt trước vào đây</mat-card-title>
+            </mat-card-header>
+            <mat-card-content>
+              <p>Kích thước ảnh lớn hơn 640 x 480</p>
+              <p>Định dạng PNG, JPG, JPEG</p>
+            </mat-card-content>
+
+            <mat-card-actions>
+              <button mat-raised-button mat-button color="primary"><mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="image"></mat-icon>Choose Image</button>
+            </mat-card-actions>
+          </mat-card>
+        </div>
+
+        <div class="col-span-1 ">
+          <mat-card>
+            <mat-card-header>
+              <mat-card-subtitle>
+                <div class="font-bold text-blue-600">Mặt sau</div>
+              </mat-card-subtitle>
+              <mat-card-title>Hoặc kéo ảnh mặt trước vào đây</mat-card-title>
+            </mat-card-header>
+            <mat-card-content>
+              <p>Kích thước ảnh lớn hơn 640 x 480</p>
+              <p>Định dạng PNG, JPG, JPEG</p>
+            </mat-card-content>
+
+            <mat-card-actions>
+              <button mat-raised-button mat-button color="primary"><mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="image"></mat-icon>Choose Image</button>
+            </mat-card-actions>
+          </mat-card>
+        </div>
+      </div>
+
+      <div class="my-2 flex flex-row space-x-4 float-right">
+        <button mat-raised-button mat-button color="basic" (click)="closeDialog()">Cancel</button>
+        <button mat-raised-button mat-button color="primary">Continue</button>
+      </div>
+    </div>
+  `,
+  standalone: true,
+  imports: [MatDialogTitle, MatDialogContent, MatButtonModule, MatIconModule, MatCardModule, MatRippleModule, MatListModule],
+})
+export class DialogDataExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogDataExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+}
+
+export interface DialogData {
+  animal: 'panda' | 'unicorn' | 'lion'
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
-import {provideAnimations} from "@angular/platform-browser/animations";
+import { routes } from './app.routes'
+import { provideAnimations } from '@angular/platform-browser/animations'
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from "@angular/material/form-field";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
-};
+  providers: [provideRouter(routes), provideAnimations(),  {
+    provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+    useValue: {
+      subscriptSizing: 'dynamic'
+    }
+  }],
+}
